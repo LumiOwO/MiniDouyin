d5c460e3a6cb7aabf70ef6ad471438f642f1e94d
chenzhuo <chenzhuo757@zju.edu.cn>
2019-07-14 17:46:10 +0800

    update video fragment

--- app/src/main/java/com/example/minidouyin/MainActivity.java
+++ app/src/main/java/com/example/minidouyin/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.minidouyin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+	private static final int PAGE_COUNT = 5;
+
+	private ViewPager mViewPager;
+	private AppBarLayout mAppBar;
+	private TabLayout mTabLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mViewPager = findViewById(R.id.vp_main);
+		mAppBar = findViewById(R.id.ab_main);
+		mTabLayout = findViewById(R.id.tl_main);
+
+		mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+			@Override
+			public Fragment getItem(int position) {
+				return new VideoFragment();
+			}
+
+			@Override
+			public int getCount() {
+				return PAGE_COUNT;
+			}
+
+			@Nullable
+			@Override
+			public CharSequence getPageTitle(int position) {
+				return super.getPageTitle(position);
+			}
+		});
+
+		mTabLayout.setupWithViewPager(mViewPager);
 	}
 }
---
+++ app/src/main/java/com/example/minidouyin/net/PostVideoResponse.java
@@ -0,0 +1,7 @@
+package com.example.minidouyin.net;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PostVideoResponse {
+	@SerializedName("success") private boolean mSuccess;
+}
